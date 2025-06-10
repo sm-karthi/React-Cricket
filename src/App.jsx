fa8a27996bf14ca5dcd688bd102f28d4fc270e01
sm-karthi <karthikeyan24022007@gmail.com>
2025-06-10 18:09:00 +0530

    Finished the timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Score from './components/Score'
 import './App.css'
 
 function App() {
-  
+
+  let [timeLeft, setTimeLeft] = useState(60);
+
+  useEffect(() => {
+
+    if (timeLeft === 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+  }, [timeLeft]);
+
 
   return (
-    <>
+    <div className='md:px-15'>
 
       <h1 className='flex justify-center mt-6 font-bold text-3xl'>CRICKET 10</h1>
 
 
-      <div className='flex justify-center items-center px-8 md:px-30'>
+      <div className='flex justify-center items-center px-8 md:px-15'>
 
         <div className='flex flex-col md:flex-row space-y-8 md:space-y-0 justify-around border border-gray-300 pt-6 pb-3 w-full mt-8 rounded-2xl shadow-lg'>
 
@@ -22,7 +36,7 @@ function App() {
 
             <h3 className='font-bold text-xl'> TIMER</h3>
 
-            <span className='font-semibold text-xl'>0</span>
+            <span className='font-semibold text-xl'>{timeLeft}</span>
 
           </div>
 
@@ -33,7 +47,60 @@ function App() {
 
       </div>
 
-    </>
+
+
+      <div className='flex justify-between w-full mt-15'>
+
+
+        <div className='flex flex-col text-center'>
+
+          <h4 className='font-semibold text-lg'>TEAM 1 SCORE BOARD</h4>
+
+          <table className="border-collapse border border-gray-400 mt-5">
+
+            <tr>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>TEAM 1</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B1</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B2</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B3</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B4</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B5</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>B6</th>
+              <th className='border border-gray-300 font-semibold px-2.5 py-1'>TOTAL</th>
+            </tr>
+
+            <tr>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'>PLAYER 1</td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+            </tr>
+
+            <tr>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'>PLAYER 2</td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+              <td className='border border-gray-300 font-semibold px-2.5 py-1'></td>
+            </tr>
+
+            <tbody></tbody>
+
+          </table>
+
+        </div>
+
+
+      </div>
+
+    </div>
   )
 }
 
